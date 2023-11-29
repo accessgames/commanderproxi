c03a519e0b07cca9a5954adc373b94c340c09cc6
accessgames <83598795+accessgames@users.noreply.github.com>
2023-11-29 11:39:31 -0800

    Bug fixing

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,11 +1,9 @@
 self.__uv$config = {
+    bare: '/bare/',
     prefix: '/service/',
-    bare: 'https://placeholder.url/bare',
-    encodeUrl: Ultraviolet.codec.plain.encode,
-    decodeUrl: Ultraviolet.codec.plain.decode,
-    handler: '/uv/uv.handler.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
-    index:  'index.js',
-    sw: '/uv/uv.sw.js',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv.handler.js',
+    bundle: '/uv.bundle.js',
+    config: '/uv.config.js',
 };
